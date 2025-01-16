07e719bd7b4fed1d5f551f536799a62e2d7842c2
starzzots <k_verwey@yahoo.com>
2025-01-16 16:26:34 -0600

    fix the game so that it takes a random list of words and # out the user picking the word to make it more fun to play that was for testing

--- hangman.rb
+++ hangman.rb
@@ -1,6 +1,5 @@
 class Hangman
   attr_reader :word, :guesses, :max_attempts
-
   def initialize(word, max_attempts = 6)
     @word = word.downcase
     @max_attempts = max_attempts
--- main.rb
+++ main.rb
@@ -7,7 +7,8 @@ end
 
 def play_game
   puts "Welcome to Hangman!"
-  word = prompt("Please enter a word to guess:")  # Get the word from the user
+  #word = prompt("Please enter a word to guess:")  # Get the word from the user
+  word = ["apple","carrot","amazing", "tomato", "mississippi", "fragile"].sample
   game = Hangman.new(word)
 
   until game.remaining_attempts.zero? || game.won?
